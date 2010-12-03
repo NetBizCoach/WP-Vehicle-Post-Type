914d3258cf6d2b6b26a436c854dd86608c734b1b
Phill Kenoyer <phill@kenoyer.com>
2010-12-03 15:22:07 -0800

    Updated some styles for inventory listings.

--- editor_popup.php
+++ editor_popup.php
@@ -23,14 +23,14 @@
     <br>
     <table width="100%" cellpadding="5" cellspacing="0" border="0">
       <tr><td align="right" width="90">Price $</td><td><input id="price_input" name="price" size="10"/></td></tr>
-      <tr><td align="right">Stock #</td><td><strong><input id="stock_input" name="stock" size="10"/></strong></td></tr>
-      <tr><td align="right">Color</td><td><strong><input id="color_input" name="color" size="10"/></strong></td></tr>
-      <tr><td align="right">Engine</td><td><strong><input id="engine_input" name="engine" size="10"/></strong></td></tr>
-      <tr><td align="right">Transmission</td><td><strong><input id="transmission_input" name="transmission" size="10"/></strong></td></tr>
-      <tr><td align="right">Mileage</td><td><strong><input id="mileage_input" name="mileage" size="10"/></strong></td></tr>
+      <tr><td align="right">Stock #</td><td><input id="stock_input" name="stock" size="10"/></td></tr>
+      <tr><td align="right">Color</td><td><input id="color_input" name="color" size="30"/></td></tr>
+      <tr><td align="right">Engine</td><td><input id="engine_input" name="engine" size="30"/></td></tr>
+      <tr><td align="right">Transmission</td><td><input id="transmission_input" name="transmission" size="30"/></td></tr>
+      <tr><td align="right">Mileage</td><td><input id="mileage_input" name="mileage" size="10"/></td></tr>
       <tr><td colspan="2">Description<br><textarea id="description_input" name="description" cols="60" rows="5"></textarea></td></tr>
       <tr><td colspan="2">Equipment<br><textarea id="equipment_input" name="equipment" cols="60" rows="5"></textarea></td></tr>
-      <tr><td colspan="2">Contact<br><textarea id="contact_input" name="contact" cols="60" rows="5"></textarea></td></tr>
+      <tr><td colspan="2">Contact<br><textarea id="contact_input" name="contact" cols="60" rows="2"></textarea></td></tr>
     </table>
     <p style="text-align:right">
       <button type="button" onclick="add_template();">Update Vehicle</button>
@@ -65,7 +65,7 @@
       
       function build_vehicle() {
         vehicle  = '<p>[gallery link="file" columns="5"]</p>';
-        vehicle += '<p id="vehicle">';
+        vehicle += '<p class="vehicle-detail">';
         vehicle += '<label>Price:</label>$<span id="price">' + document.getElementById('price_input').value + '</span><br>';
         vehicle += '<label>Stock:</label>#<span id="stock">' + document.getElementById('stock_input').value + '</span><br>';
         vehicle += '<label>Color:</label><span id="color">' + document.getElementById('color_input').value + '</span><br>';
@@ -76,15 +76,15 @@
         if (document.getElementById('mileage_input').value)
           vehicle += '<label>Mileage:</label><span id="mileage">' + document.getElementById('mileage_input').value + '</span><br>';
         vehicle += '</p>';
-        vehicle += '<p>Description:<br><span id="description">' + document.getElementById('description_input').value + '</span></p>';
-        vehicle += '<p>Equipment:<br><span id="equipment">' + document.getElementById('equipment_input').value + '</span></p>';
+        vehicle += '<p class="vehicle-detail"><label>Description:</label><br><span id="description">' + document.getElementById('description_input').value + '</span></p>';
+        vehicle += '<p class="vehicle-detail"><label>Equipment:</label><br><span id="equipment">' + document.getElementById('equipment_input').value + '</span></p>';
         if (document.getElementById('contact_input').value)
-          vehicle += '<p>Contact:<br><span id="contact">' + document.getElementById('contact_input').value + '</span></p>';
+          vehicle += '<p class="vehicle-detail"><label>Contact:</label><br><span id="contact">' + document.getElementById('contact_input').value + '</span></p>';
         return vehicle;
       }
       
       function build_excerpt() {
-        excerpt += '<p class="vehicle">';
+        excerpt += '<p class="vehicle-listing">';
         excerpt += '<span class="excerpt-price">$' + document.getElementById('price_input').value + '</span><br>';
         excerpt += '<label>Stock:</label>#<span class="excerpt-stock">' + document.getElementById('stock_input').value + '</span>, ';
         excerpt += '<label>Color:</label><span class="excerpt-color">' + document.getElementById('color_input').value + '</span>, ';
